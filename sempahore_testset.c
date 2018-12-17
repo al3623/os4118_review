229b8c3cb1a42aa4eae8f49b56a71cbd5a143e87
Amanda Liu <al3623@columbia.edu>
2018-12-16 20:33:15 -0500

    More semaphore examples

--- sempahore_testset.c
+++ sempahore_testset.c
@@ -1,22 +1,29 @@
-/* MUST BE EXECUTED ATOMICALLY */
 
 struct semaphore = {
 	struct wait_queue queue;
 	int value;
 };
 
-void wait(struct semaphore *S) {
+void wait(S) {
+	while(test_and_set(&value_lock));
 	S->value--;
 	if (S->value < 0) {
 		S->wait_queue.add(current);
+		value_lock = 0;
 		block();
+	} else {
+		value_lock = 0;
 	}
 }
 
 void signal(S) {
+	while(test_and_set(&value_lock));
 	S->value++;
 	if (S->value <= 0) {
 		P = S->wait_queue.dequeue();
+		value_lock = 0;
 		wake(P);
-	}	
+	} else {
+		value_lock = 0;
+	}
 }
